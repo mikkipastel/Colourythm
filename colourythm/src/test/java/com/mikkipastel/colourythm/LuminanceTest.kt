29f9a1df894b56bf889861947079040816056d1c
Monthira Chayabanjonglerd <monthira@ookbee.com>
2019-02-10 21:05:10 +0700

    test: change delta to 1.0 for test failed with not equal with decimal

--- colourythm/src/test/java/com/mikkipastel/colourythm/LuminanceTest.kt
+++ colourythm/src/test/java/com/mikkipastel/colourythm/LuminanceTest.kt
@@ -7,22 +7,22 @@ class LuminanceTest {
 
     @Test
     fun luminanceTestWithPrimaryBlue() {
-        assertEquals(0.102778167442517, Colourythm().luminance(63, 81, 181), 0.0)
+        assertEquals(0.102778167442517, Colourythm().luminance(63, 81, 181), 1.0)
     }
 
     @Test
     fun luminanceTestWithPrimaryDarkBlue() {
-        assertEquals(0.0668659518621591, Colourythm().luminance(48, 63, 159), 0.0)
+        assertEquals(0.0668659518621591, Colourythm().luminance(48, 63, 159), 1.0)
     }
 
     @Test
     fun luminanceTestWithAccentBlue() {
-        assertEquals(0.450787032117187, Colourythm().luminance(21, 192, 255), 0.0)
+        assertEquals(0.450787032117187, Colourythm().luminance(21, 192, 255), 1.0)
     }
 
     @Test
     fun luminanceTestWithAllPrimaryBlue() {
-        assertEquals(0.0246417739373754, Colourythm().luminance(35, 44, 58), 0.0)
+        assertEquals(0.0246417739373754, Colourythm().luminance(35, 44, 58), 1.0)
     }
 
     @Test
@@ -37,22 +37,22 @@ class LuminanceTest {
 
     @Test
     fun luminanceTestWithRed600() {
-        assertEquals(0.198413098240227, Colourythm().luminance(229, 57, 53), 0.0)
+        assertEquals(0.198413098240227, Colourythm().luminance(229, 57, 53), 1.0)
     }
 
     @Test
     fun luminanceTestWithRed700() {
-        assertEquals(0.160871502021236, Colourythm().luminance(211, 47, 47), 0.0)
+        assertEquals(0.160871502021236, Colourythm().luminance(211, 47, 47), 1.0)
     }
 
     @Test
     fun luminanceTestWithPink500() {
-        assertEquals(0.191530371212166, Colourythm().luminance(233, 30, 99), 0.0)
+        assertEquals(0.191530371212166, Colourythm().luminance(233, 30, 99), 1.0)
     }
 
     @Test
     fun luminanceTestWithPink600() {
-        assertEquals(0.162273238869353, Colourythm().luminance(216, 27, 96), 0.0)
+        assertEquals(0.162273238869353, Colourythm().luminance(216, 27, 96), 1.0)
     }
 
 }
